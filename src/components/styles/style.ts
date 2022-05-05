e8e07c7209f6c262acf143f552ad973fa279ec9a
Axlii <axlii50@gmail.com>
2022-05-05 03:45:01 +0200

    integrated fetch

---
+++ src/components/styles/style.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create(
+{
+    title:
+    {
+        fontSize: 25,
+        margin: 15,
+        textAlign: 'center'
+    },
+    header: 
+    {
+        backgroundColor: "skyblue",
+        margin: 5
+    },
+    course:
+    {
+        fontSize: 16,
+        padding: 7
+    },
+    actionButtonIcon: 
+    {
+        fontSize: 20,
+        height: 22,
+        color: 'white',
+    }
+});
+
+export default Styles;
--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,81 +1,16 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
-import { Colors, DebugInstructions, Header } from 'react-native/Libraries/NewAppScreen';
+import { Colors, DebugInstructions } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<{title: string; }> = ({children, title}) => 
-  {
-    const isDarkMode = useColorScheme() === 'dark';
-    return (
-      <View style={styles.sectionContainer}>
-        <Text
-          style={[
-            styles.sectionTitle,
-            {
-              color: isDarkMode ? Colors.white : Colors.black,
-            },
-          ]}>
-          {title}
-        </Text>
-        <Text
-          style={[
-            styles.sectionDescription,
-            {
-              color: isDarkMode ? Colors.light : Colors.dark,
-            },
-          ]}>
-          {children}
-        </Text>
-      </View>
-    );
-  };
+import GroupView from './GroupView';
 
 const Home = () => 
 {
     return (
-        <View>
-            <ScrollView contentInsetAdjustmentBehavior="automatic" style={{ backgroundColor: Colors.lighter }}>
-                <Header/>
-                <View
-                style={{
-                    backgroundColor: Colors.lighter
-                }}>
-                <Section title="Gedon">
-                    Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                    screen and then come back to see your edits.
-                </Section>
-                <Section title="See Your xxxxChanges">
-                    xDDD
-                </Section>
-                <Section title="Gedon">
-                    <DebugInstructions />
-                </Section>
-                <Section title="Gedon">
-                    Read the docs to discover what to do next:
-                </Section>
-                </View>
-            </ScrollView>
+        <View style={{ backgroundColor: Colors.lighter }}>
+            <GroupView></GroupView>
         </View>
     );
 };
-
-const styles = StyleSheet.create(
-    {
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-        highlight: {
-        fontWeight: '700',
-    }
-});
 
 export default Home;
