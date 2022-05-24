144020cfbc1c05bdb05cb7e0bbaede20c7b809b4
Axlii <axlii50@gmail.com>
2022-05-25 01:59:01 +0200

    schedule component init

---
+++ src/components/views/Schedule.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { Colors, DebugInstructions } from 'react-native/Libraries/NewAppScreen';
+
+const Schedule = ({ route, navigation }) => 
+{
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+};
+
+export default Schedule;
